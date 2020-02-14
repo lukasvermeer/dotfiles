74da5e78dcd09dca26cee350e674ad657de14c71
Lukas Vermeer <lukas@lukasvermeer.nl>
2020-02-14 14:10:20 +0100

    Update opensesame.sh

--- bin/opensesame.sh
+++ bin/opensesame.sh
@@ -1,2 +1,6 @@
+# Assumes the following is in /etc/fstab
+# UUID=5DACC3692D2D990F	/mnt/Red	ntfs-3g	defaults,uid=1000,gid=1000,umask=002,errors=continue,noauto 0 0
+# /dev/mapper/blue	/mnt/Blue	ext4	defaults,noauto,user	  0	  0
+
 echo "Da ba dee da ba dai"
 sudo cryptsetup luksOpen /dev/sda1 blue && mount /mnt/Blue && ls -lah /mnt/Blue
